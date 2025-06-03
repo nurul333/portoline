e42daaa887cce27069f78945563ef1c3ec5e3307
nurul333 <nurulfadillah10des@gmail.com>
2025-06-03 22:20:05 +0700

    Fix NDK version in build.gradle.kts and resolve build error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,13 @@
+// Tambahkan ini paling atas atau sebelum allprojects
+ext.set("flutter", mapOf(
+    "compileSdkVersion" to 34,
+    "minSdkVersion" to 21,
+    "targetSdkVersion" to 34,
+    "versionCode" to 1,
+    "versionName" to "1.0",
+    "ndkVersion" to "27.0.12077973"
+))
+
 allprojects {
     repositories {
         google()
@@ -8,23 +18,6 @@ allprojects {
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-
-android {
-    compileSdk = 34
-    namespace = "com.yourcompany.portoline"
-
-    ndkVersion = "27.0.12077973" // Tambahkan ini di sini
-
-    defaultConfig {
-        applicationId = "com.yourcompany.portoline"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    // dll...
-}
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
