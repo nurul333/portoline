a0e42f890fdefeeecbb8065ba4cf2da634a9d3b1
nurul333 <nurulfadillah10des@gmail.com>
2025-06-03 22:03:26 +0700

    Fix AndroidManifest package attribute and set correct NDK version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,9 +5,14 @@ allprojects {
     }
 }
 
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+android {
+    ndkVersion = "27.0.12077973"
+    // yang lain tetap
+}
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
