38b58b06f149fce6186ef3fc5f669aaa33ff1e06
nurul333 <nurulfadillah10des@gmail.com>
2025-06-03 22:10:56 +0700

    Fix NDK version in build.gradle.kts and resolve build error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,9 +10,22 @@ val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build"
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 android {
-    ndkVersion = "27.0.12077973"
-    // yang lain tetap
+    compileSdk = 34
+    namespace = "com.yourcompany.portoline"
+
+    ndkVersion = "27.0.12077973" // Tambahkan ini di sini
+
+    defaultConfig {
+        applicationId = "com.yourcompany.portoline"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    // dll...
 }
+
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
